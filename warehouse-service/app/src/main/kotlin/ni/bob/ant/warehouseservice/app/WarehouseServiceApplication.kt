4440e35b1da8fa6e96b179ec9bb9e3d423e67e23
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-10-20 02:39:40 +0300

    fix bug with component scan packages
    Signed-off-by: Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>

--- warehouse-service/app/src/main/kotlin/ni/bob/ant/warehouseservice/app/WarehouseServiceApplication.kt
+++ warehouse-service/app/src/main/kotlin/ni/bob/ant/warehouseservice/app/WarehouseServiceApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan(basePackages = ["ni.bob.ant.warehouseservice.usecase.usecase"])
+@ComponentScan(basePackages = ["ni.bob.ant.warehouseservice.usecase.usecase", "ni.bob.ant.warehouseservice.app"])
 @SpringBootApplication
 class WarehouseServiceApplication
 
