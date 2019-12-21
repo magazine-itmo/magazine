349866dd20b8faae684a7616bb176cb16f0f4f08
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-12-21 15:55:43 +0300

    Order service // send unbooking message to messageBroker

--- order-service/usecase/src/main/kotlin/ni/bob/ant/orderservice/usecase/usecase/PerformPaymentUseCase.kt
+++ order-service/usecase/src/main/kotlin/ni/bob/ant/orderservice/usecase/usecase/PerformPaymentUseCase.kt
@@ -7,10 +7,13 @@ import ni.bob.ant.orderservice.usecase.conf.UseCase
 import ni.bob.ant.orderservice.usecase.dto.OrderDto
 import ni.bob.ant.orderservice.usecase.dto.toDto
 import ni.bob.ant.orderservice.usecase.exceptions.notFound
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
 
 @UseCase
 class PerformPaymentUseCase(
-        val paymentRepository: PaymentRepository
+        val paymentRepository: PaymentRepository,
+        val messageDistributor: MessageDistributor
 ) {
     fun execute(orderId: Long): OrderDto {
         val order = paymentRepository.findOrderById(Identity(orderId)) ?: notFound<Order>(orderId)
@@ -20,6 +23,9 @@ class PerformPaymentUseCase(
                 order.toState(OrderState.Paid)
             } else {
                 order.toState(OrderState.Failed)
+                for (orderItem in order.orderItems) {
+                    messageDistributor.send(orderItem.identity.value, orderItem.quantity)
+                }
             }
         }
         return paymentRepository.save(order).toDto()
@@ -30,6 +36,22 @@ class PerformPaymentUseCase(
         // I'm not sure the payment performing should be declared in repository,
         // but mne posrat' because it's really convenient in this case
         fun performPayment(identity: Identity, totalCost: Long): Boolean
+
         fun save(order: Order): Order
     }
+
+
+    @Component
+    class MessageDistributor(
+            val kafkaTemplate: KafkaTemplate<String, Message>
+    ) {
+        fun send(itemId: Long, quantity: Int) {
+            kafkaTemplate.send("Orders", Message(itemId, quantity))
+        }
+    }
+
+    data class Message(
+            val itemId: Long,
+            val quantity: Int
+    )
 }
